81f203b0f1519a57cafec40044bdae947635a54c
ju4n_0 <juanswc87@gmail.com>
2026-03-07 00:58:00 -0300

    Readme

---
+++ src/main/java/com/gestor/game/GestorItemsUserApplication.java
@@ -0,0 +1,13 @@
+package com.gestor.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorItemsUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorItemsUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestor/game/GestorItemsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestor.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorItemsUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
